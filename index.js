299551cf460a0de579379996970222ce4858e469
tonyoconnor61 <tonyoc_1@yahoo.com>
2018-12-30 12:28:02 +0000

    fix port variable in index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 5000;
+const server_port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(server_port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${server_port}/`);
 });
